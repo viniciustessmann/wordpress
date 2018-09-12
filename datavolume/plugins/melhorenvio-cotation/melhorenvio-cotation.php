f3164abf7a47aeb8acdc3c40ccf6869f51fb0932
Samuel <samuel@melhorenvio.com.br>
2018-09-12 11:29:51 -0300

    New plugin init

---
+++ datavolume/plugins/melhorenvio-cotation/melhorenvio-cotation.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:  Melhor Envio Cotation
+Plugin URI:   https://www.melhorenvio.com.br
+Description:  Plugin de cotações do Melhor Envio para WooCommerce
+Version:      2.0.0
+Author:       Melhor Envio
+Author URI:   https://www.melhorenvio.com.br
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
+
